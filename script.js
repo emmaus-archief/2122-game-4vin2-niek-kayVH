7b831fdf4a05916907f23b9f0b1da6b0e25a3910
nieknicolle <112803@emmauscollege.nl>
2022-06-08 09:30:51 +0000

    opnieuw

--- script.js
+++ script.js
@@ -151,6 +151,7 @@ function draw() {
     fill('white');
     text('Uitleg: Enter To Restart', 150, 150);
     if (keyIsDown(13)) {
+      vijandX = [3000, 2800, 2600, 2400, 2200, 1000, 8000, 600, 400];
     spelStatus = SPELEN;
      }
   }
